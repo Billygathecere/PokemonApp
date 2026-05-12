5f9f68b7f831efe29b988f31b4c18695c437b3e3
Billy Gathecere <bgathecere2@gmail.com>
2026-05-12 12:07:56 +0300

    chore: add package.json for react-icons wi module with sideEffects set to false

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 function App() {
+
+  // Search Bar Logic
+  // useState will store the text in the search bar and update the query value
+  const [query, setQuery] = useState("");
+
+
   return (
     <div>
-      <h1>🎮 Pokemon App</h1>
-      <p>Welcome to your Pokemon App! React is working.</p>
+      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Search Pokemon" />
+      {/* the handleSearch function will be called when the button is clicked */}
+      <button onClick={handleSearch}>Search</button>
+      <h1>Pokemon Universe</h1>
+      <p>Delve into the world of Pokemon, learn about'em and find your favorites!</p>
     </div>
   );
 }
